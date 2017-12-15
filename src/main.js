6259b67c5cd4f0952dbf351d80cf4bc13aa725a3
Raegan Millhollin <desdemonhu@gmail.com>
2017-12-15 01:31:29 -0800

    Notes render on the page! Delete does nothing yet

--- src/main.js
+++ src/main.js
@@ -1,5 +1,79 @@
 import './style/main.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import uuid from 'uuid';
 
-console.log('Cool beans');
+class Header extends React.Component {
+  render(){
+    return (
+      <h1>Welcome to Notepad</h1>
+    )
+  }
+}
+
+class CreateNoteForm extends React.Component {
+  constructor(content){
+    super(content);
+    this.state = {value: '',
+                  list: []
+                };
+
+    this.addNote = this.addNote.bind(this);
+    this.updateState = this.updateState.bind(this);
+  }
+
+  addNote(){
+    let note =
+      {
+        id: uuid.v1(),
+        editing: false,
+        completed: false,
+        content: this.state.value
+      }
+    this.setState(state => {
+      return {list: this.state.list.concat(note)};
+    });
+  }
+
+  updateState(event){
+    this.setState({value: event.target.value})
+  }
+
+  render(){
+    return (
+      <div>
+        <input type="text"
+          name="note"
+          value={this.state.value}
+          onChange={this.updateState}>
+        </input>
+        <button onClick={this.addNote}>Add Note</button>
+        <ul>
+          {this.state.list.map((note) =>
+            (
+              <li>
+              <h3>Note: {note.id}</h3>
+              <p>{note.content}</p>
+              <button>Delete Note</button>
+            </li>
+          )
+        )}
+        </ul>
+      </div>
+    )
+  }
+}
+
+
+class App extends React.Component {
+  render(){
+    return (
+      <div>
+        <Header />
+        <CreateNoteForm />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
