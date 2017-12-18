0f5775a283da5f8550a239ebee8079ee7862c7b6
Raegan Millhollin <desdemonhu@gmail.com>
2017-12-17 20:25:02 -0800

    Added update form

--- src/main.js
+++ src/main.js
@@ -58,14 +58,59 @@ class CreateNoteForm extends React.Component {
 class NoteItem extends React.Component {
   constructor(props){
     super(props)
+    this.state = {showForm: false}
   }
+
+  openForm = (event) => {
+    event.preventDefault()
+    this.setState({showForm: true})
+  }
+
+  removeForm = (event) => {
+    event.preventDefault()
+    this.setState({showForm: false})
+  }
+
   render(){
     return (
-        <li>
+        <li id="note_{this.props.note.id}">
         <h3>Note: {this.props.note.id}</h3>
         <p>{this.props.note.content}</p>
         <button value={this.props.note.id} onClick={this.props.deleteNote}>Delete Note</button>
+        <button  onClick={this.openForm}>Edit Note</button>
+        <div id="form_{this.props.note.id}">
+        {this.state.showForm === true &&
+          <NoteUpdateForm note={this.props.note} noteUpdateField={this.props.noteUpdateField} removeForm={this.removeForm}/>
+        }
+        </div>
       </li>
+    )
+  }
+}
+
+class NoteUpdateForm extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {value: ''}
+  }
+
+  updateFormField = (event) => {
+    this.setState({value: event.target.value})
+  }
+
+  updateNoteContent = (event) => {
+    event.preventDefault()
+    console.log(event.target.value);
+    this.props.noteUpdateField(event.target.value, this.state.value)
+  }
+
+  render(){
+    return (
+      <form>
+        <input value={this.state.value} onChange={this.updateFormField}/>
+        <button value={this.props.note.id} onClick={this.updateNoteContent}>Update</button>
+        <button onClick={this.props.removeForm}>Cancel Update</button>
+      </form>
     )
   }
 }
@@ -91,6 +136,19 @@ postNote = (note) => {
     })
   }
 
+  noteUpdateField = (noteid, updatedContent) => {
+    let index = this.state.list.findIndex(note => {
+      return note.id === noteid
+    })
+    console.log(index);
+    if(index > -1){
+      this.setState(state => {
+        this.state.list[index].content = updatedContent
+        return {list: this.state.list}
+      })
+    }
+  }
+
   render(){
     return (
       <div>
@@ -98,7 +156,7 @@ postNote = (note) => {
         <CreateNoteForm postNote={this.postNote}/>
         <ul>
           {this.state.list.map((note) =>
-            <NoteItem  note={note} deleteNote={this.deleteNote}/>
+            <NoteItem  note={note} deleteNote={this.deleteNote} noteUpdateField={this.noteUpdateField} upd/>
         )}
         </ul>
       </div>
